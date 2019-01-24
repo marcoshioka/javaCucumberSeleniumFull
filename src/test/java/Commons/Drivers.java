a1076842efcc92e8c0317845d2e79a29d38cfe37
Marcos Ribeiro Hioka <mribeirh@everis.com>
2019-01-24 15:53:33 -0200

    24/01/19 - Adicionado argumento --no-sandbox para execução no Jenkins

--- src/test/java/Commons/Drivers.java
+++ src/test/java/Commons/Drivers.java
@@ -71,6 +71,7 @@ public class Drivers {
 			 * mobileEmulation.put("deviceName", "iPhone X");
 			 * options.setExperimentalOption("mobileEmulation", mobileEmulation);
 			 */
+			options.addArguments("--no-sandbox");
 			options.addArguments("--disable-dev-shm-usage");
 			options.addArguments("--disable-notifications");
 			options.addArguments("disable-infobars");
