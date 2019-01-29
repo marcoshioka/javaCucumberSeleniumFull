d17afa648950e28b69b5aaf364556545ea7d4609
Carlos Almeida <carloscgta@gmail.com>
2019-01-29 19:30:11 +0000

    Foi descomentado os métodos: pdfGenerator

--- src/test/java/Steps/LoginSteps.java
+++ src/test/java/Steps/LoginSteps.java
@@ -18,12 +18,12 @@ public class LoginSteps extends LoginPage {
 	
 	@Before(value = "@login", order = 1)
 	public void before(Scenario scenario) throws Exception {
-	//	pdfGenerator.iniciaPDF(scenario);
+		pdfGenerator.iniciaPDF(scenario);
 	}
 
 	@After(value = "@login", order = 1)
 	public void finalizaPDF(Scenario scenario) throws Exception {
-		//	pdfGenerator.fechaPDF(scenario);
+			pdfGenerator.fechaPDF(scenario);
 	}
 
 	@After(value = "@login", order = 0)
@@ -35,7 +35,7 @@ public class LoginSteps extends LoginPage {
 	public void que_eu_esteja_na_página_de_login_da_Pi() throws Throwable {
 		String name = "Dado que eu esteja na página de login da Pi";
 		LoginPage.irParaLogin();
-		//	pdfGenerator.conteudoPDF(name);
+			pdfGenerator.conteudoPDF(name);
 		hooks.getEvidence();
 	}
 
@@ -43,7 +43,7 @@ public class LoginSteps extends LoginPage {
 	public void eu_insiro_meu_e_mail_cadastrado() throws Throwable {
 		String name = "Quando eu insiro meu e-mail cadastrado";
 		LoginPage.inserirEmail();
-		//	pdfGenerator.conteudoPDF(name);
+			pdfGenerator.conteudoPDF(name);
 		hooks.getEvidence();
 	}
 
@@ -51,7 +51,7 @@ public class LoginSteps extends LoginPage {
 	public void minha_senha() throws Throwable {
 		String name = "E minha senha";
 		LoginPage.inserirSenha();
-		//	pdfGenerator.conteudoPDF(name);
+			pdfGenerator.conteudoPDF(name);
 		hooks.getEvidence();
 	}
 
@@ -59,7 +59,7 @@ public class LoginSteps extends LoginPage {
 	public void clico_no_botão_de_Login() throws Throwable {
 		String name = " E clico no botão de Login";
 		LoginPage.clicarBotaoLogin();
-		//	pdfGenerator.conteudoPDF(name);
+			pdfGenerator.conteudoPDF(name);
 		hooks.getEvidence();
 	}
 
@@ -67,7 +67,7 @@ public class LoginSteps extends LoginPage {
 	public void eu_devo_logar_com_sucesso_na_aplicação() throws Throwable {
 		String name = "Então eu devo logar com sucesso na aplicação";
 		LoginPage.confirmarLogin();
-		//	pdfGenerator.conteudoPDF(name);
+			pdfGenerator.conteudoPDF(name);
 		hooks.getEvidence();
 	}
 
@@ -75,7 +75,7 @@ public class LoginSteps extends LoginPage {
 	public void eu_insiro_um_não_cadastrado(String emailTabela) throws Throwable {
 		String name = "Quando eu insiro um '<email>' não cadastrado";
 		LoginPage.inserirEmailNaoCadastrado(emailTabela);
-		//	pdfGenerator.conteudoPDF(name);
+			pdfGenerator.conteudoPDF(name);
 		hooks.getEvidence();
 	}
 
@@ -83,7 +83,7 @@ public class LoginSteps extends LoginPage {
 	public void uma_qualquer(String senhaTabela) throws Throwable {
 		String name = "E uma '<senha>' qualquer";
 		LoginPage.inserirSenhaNaoCadastrada(senhaTabela);
-		//	pdfGenerator.conteudoPDF(name);
+			pdfGenerator.conteudoPDF(name);
 		hooks.getEvidence();
 	}
 
@@ -91,7 +91,7 @@ public class LoginSteps extends LoginPage {
 	public void eu_não_devo_logar() throws Throwable {
 		String name = "Então eu não devo logar";
 		LoginPage.tentarLogarSemSucesso();
-		//	pdfGenerator.conteudoPDF(name);
+			pdfGenerator.conteudoPDF(name);
 		hooks.getEvidence();
 	}
 
